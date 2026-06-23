cc3d39d523d631e5172451ee895688f22b95e7f0
ChefNek2025 <hailu.nekatibeb2025@gmail.com>
2026-06-22 20:13:43 -0400

    Fix pricing page checkout function

--- fix-pricing.js
+++ fix-pricing.js
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+const fs = require('fs');
+
+const code = `import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { supabase } from '../lib/supabase';
 
@@ -109,4 +111,7 @@ export default function PricingPage() {
       </div>
     </div>
   );
-}
+}`;
+
+fs.writeFileSync('pages/pricing.tsx', code);
+console.log('done!');
