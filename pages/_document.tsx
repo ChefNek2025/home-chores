8c18206ca9a6508227bcdcee33319726cc4173f7
ChefNek2025 <hailu.nekatibeb2025@gmail.com>
2026-07-19 12:39:50 -0400

    Add Apple touch icon for iPhone PWA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="fo-verify" content="7b97d469-4fad-4c43-b149-8bceaf6862b0" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="Seru Chores" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </Head>
